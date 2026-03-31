9d98e839f54db0d6536ad72190cdaf01f68bb89d
daniil karpushko <d.karpushko@tbank.ru>
2026-03-31 20:56:09 +0300

    fix: config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+      allowedHosts: [
+          'best-girl-irl.onrender.com',
+          '.onrender.com',
+          'localhost'
+      ]},
   plugins: [react()],
+
 })
